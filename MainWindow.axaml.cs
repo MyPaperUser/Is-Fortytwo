688b56f9316ab240c15223a94040589998c7562d
Elias <elias.fierke@outlook.com>
2024-12-10 14:43:24 +0100

    implemented massive highly complex logic

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,11 @@
 using Avalonia.Controls;
+using System.IO;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
+using System.Reflection;
 
 namespace is_fortytwo;
 
@@ -7,5 +14,24 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void Button_Exit_Click(object sender, RoutedEventArgs e){
+        Environment.Exit(-1);
+    }
+    public void Button_Click(object sender, RoutedEventArgs e){
+            GrdResult.IsVisible = true;
+            GrdEdit.IsVisible = false;
+        try{
+            if(Convert.ToInt32(TbNumber.Text) == 42){
+            
+                LblResult.Content = "Congratulations!\nYour number is equal to 42!";
+            } else {
+                LblResult.Content = "Sadly, your number is NOT equal to 42! :(";
+            }
+        } catch {
+            LblResult.Content = "You did it wrong!\nPlease enter a NUMBER, nothing else...";
+        }
+        
     }
 }
